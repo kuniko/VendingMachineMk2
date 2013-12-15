66a32553a2e88c496d9591a7b7510ae315a717d3
t2-kob <t2k@livw.jp>
2013-12-15 23:54:39 +0900

    ファイル足りてなかった。

---
+++ VendingMachineMk2/VendingMachineMk2/Core/VendingCore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendingMachineMk2.Data;
+
+namespace VendingMachineMk2.Core {
+
+    /// <summary>
+    /// UIからは、常時このクラス『のみ』を呼ぶ！
+    /// 呼び出された本クラスは、個々のManagerを組み合わせて処理をする旗振り役となる。
+    /// 
+    /// このクラスから呼ぶViewModelがほしいな～
+    /// </summary>
+    class VendingCore {
+        static VendingCore _instance;
+
+        private VendingMachine _ui;
+
+        private VendingCore() { }
+
+        public static VendingCore GetInstance() {
+            if (_instance == null) {
+                _instance = new VendingCore();
+            }
+
+            return _instance;
+        }
+
+        public void InsertYen(int insertedYen) {
+            MoneyManager moneyManager = MoneyManager.GetInstance();
+            int totalYen = moneyManager.InsertYen(insertedYen);
+
+            CanBuyShohin(totalYen);
+        }
+
+        public void CanBuyShohin(int totalYen) {
+            StockManager stockManager = StockManager.GetInstance();
+
+            foreach (Shohin shouhin in ShohinMaster.ShouhinCatalogue()) {
+                bool canBuy = stockManager.CanBuy(shouhin.ShohinCode, totalYen);
+
+                if (canBuy) { 
+                }
+            }
+
+        }
+
+        public void PushShohinButton(string shohinCode) { 
+            
+        }
+
+        public void Reflesh() { 
+
+        }
+
+    }
+}
---
+++ VendingMachineMk2/VendingMachineMk2/Data/Shohin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachineMk2.Data {
+   public  class Shohin {
+        public Shohin(string shohinCode, string shohinName, int sellingYen) {
+            this.ShohinCode = shohinCode;
+            this.ShohinName = shohinName;
+            this.SellingYen = sellingYen;
+        }
+
+        public int SellingYen { get; set; }
+        public string ShohinCode { get; set; }
+        public string ShohinName { get; set; }
+    }
+}
